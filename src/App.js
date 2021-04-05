8e8289560e2e791f6e178db7a02f1b9464d0955e
Mo Rob <31892724+morob1@users.noreply.github.com>
2021-04-05 15:24:12 +0100

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
     </div>
   );
 }
